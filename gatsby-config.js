fde7f9adf9a629808d691f4bd58e8d4f70601035
Lukas Zanner <git@lukaszanner.de>
2020-02-14 22:06:34 +0100

    Remove unnecessary plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,38 +7,38 @@ module.exports = {
         siteUrl: config.siteUrl,
     },
     plugins: [
-        `gatsby-plugin-netlify-cms`,
-        `gatsby-plugin-postcss`,
-        `gatsby-plugin-react-helmet`,
-        `gatsby-plugin-sharp`,
-        `gatsby-transformer-sharp`,
+        'gatsby-plugin-netlify-cms',
+        'gatsby-plugin-postcss',
+        'gatsby-plugin-react-helmet',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
         {
-            resolve: `gatsby-source-filesystem`,
+            resolve: 'gatsby-source-filesystem',
             options: {
-                path: `${__dirname}/content/pages`,
-                name: `pages`,
+                path: '${__dirname}/content/pages',
+                name: 'pages',
             },
         },
         {
-            resolve: `gatsby-source-filesystem`,
+            resolve: 'gatsby-source-filesystem',
             options: {
-                path: `${__dirname}/content/blog`,
-                name: `blog`,
+                path: '${__dirname}/content/blog',
+                name: 'blog',
             },
         },
         {
-            resolve: `gatsby-source-filesystem`,
+            resolve: 'gatsby-source-filesystem',
             options: {
-                path: `${__dirname}/content/assets`,
-                name: `assets`,
+                path: '${__dirname}/content/assets',
+                name: 'assets',
             },
         },
         {
-            resolve: `gatsby-transformer-remark`,
+            resolve: 'gatsby-transformer-remark',
             options: {
                 plugins: [
-                    `gatsby-remark-copy-linked-files`,
-                    `gatsby-remark-smartypants`,
+                    'gatsby-remark-copy-linked-files',
+                    'gatsby-remark-smartypants',
                     {
                         resolve: "gatsby-remark-external-links",
                         options: {
@@ -47,34 +47,34 @@ module.exports = {
                         }
                     },
                     {
-                        resolve: `gatsby-remark-images`,
+                        resolve: 'gatsby-remark-images',
                         options: {
                             maxWidth: 590,
                         },
                     },
                     {
-                        resolve: `gatsby-remark-responsive-iframe`,
+                        resolve: 'gatsby-remark-responsive-iframe',
                         options: {
-                            wrapperStyle: `margin-bottom: 1.0725rem`,
+                            wrapperStyle: 'margin-bottom: 1.0725rem',
                         },
                     },
                 ],
             },
         },
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `EnjoyIngredients - the other food blog`,
-                short_name: `EnjoyIngredients`,
-                start_url: `/`,
-                background_color: `#ffffff`,
-                theme_color: `#663399`,
-                display: `minimal-ui`,
-                icon: `content/assets/gatsby-icon.png`,
-            },
-        },
+        // {
+        //     resolve: 'gatsby-plugin-manifest',
+        //     options: {
+        //         name: 'EnjoyIngredients - the other food blog',
+        //         short_name: 'EnjoyIngredients',
+        //         start_url: '/',
+        //         background_color: '#ffffff',
+        //         theme_color: '#663399',
+        //         display: 'minimal-ui',
+        //         icon: 'content/assets/gatsby-icon.png',
+        //     },
+        // },
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
-        // `gatsby-plugin-offline`,
+        // 'gatsby-plugin-offline',
     ],
 }
