7fcc0483440e18bde0935f2ffb251a442cbe685d
Lukas Zanner <git@lukaszanner.de>
2020-02-23 17:04:19 +0100

    Move css file

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const { createFilePath } = require('gatsby-source-filesystem');
-// const replacePath = path => (path === `/` ? path : path.replace(/\/$/, ''));
 
 exports.createPages = async ({ actions, graphql }) => {
     const { createPage } = actions;
@@ -109,28 +108,13 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     }
 }
 
-// TODO
-// Implement the Gatsby API “onCreatePage”. This is called after every page is created.
-// exports.onCreatePage = ({ page, actions }) => {
-//     const { createPage, deletePage } = actions;
-//     const oldPage = Object.assign({}, page);
-    
-//     // Remove trailing slash unless page is /
-//     page.path = replacePath(page.path);
-//     if (page.path !== oldPage.path) {
-//         // Replace new page with old page
-//         deletePage(oldPage);
-//         createPage(page);
-//     }
-// }
-
 // Give the src folders an alias for prettier imports
 exports.onCreateWebpackConfig = ({ actions }) => {
     actions.setWebpackConfig({
         resolve: {
             alias: {
                 '~components': path.resolve(__dirname, 'src/components'),
-                '~css': path.resolve(__dirname, 'src/css'),
+                '~styles': path.resolve(__dirname, 'src/styles'),
                 '~pages': path.resolve(__dirname, 'src/pages'),
                 '~templates': path.resolve(__dirname, 'src/templates'),
                 '~utils': path.resolve(__dirname, 'src/utils'),
