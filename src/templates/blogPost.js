9f393ba47d4f0cfcbccff1e20876aacd8cb2ec2e
Lukas Zanner <git@lukaszanner.de>
2020-03-09 20:20:46 +0100

    Update style of blog post

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -26,7 +26,7 @@ export const BlogPost = ({ data: { post }, location }) => {
                 description={post.custom_excerpt || post.excerpt}
                 pathname={location.pathname}
             />
-            <article className="post-full-content">
+            <article>
                 <header>
                     <StyledPostHeading>{post.title}</StyledPostHeading>
                     <StyledPublishingDate>{post.published_at_pretty}</StyledPublishingDate>
@@ -88,7 +88,7 @@ const StyledPostContent = styled.section`
         font-size: ${theme.fontSize.xl2};
         font-weight: 500;
         padding: 2rem 0 0.3rem 0;
-        &:first-child {
+        &:first-of-type {
             padding-top: 1rem;
         }
     }
@@ -98,8 +98,146 @@ const StyledPostContent = styled.section`
         padding: 1.125rem 0 0.3rem 0;
     }
 
+    figure {
+        &.kg-card.kg-image-card {
+            padding: 1rem 0;
+            max-width: 100%;
+
+            &.kg-width-wide {
+                .kg-image {
+                    max-width: 1040px;
+                }
+
+                @media (max-width: 500px) {
+                    margin-right: -5vw;
+                    margin-left: -5vw;
+                }
+            }
+            &.kg-width-full {
+                .kg-image {
+                    position: relative;
+                    max-width: 100vw;
+                    margin: 0 -50%;
+
+                    @media (max-width: 1040px) {
+                        width: 100vw;
+                    }
+                }
+            }
+        }
+
+        &.kg-card.kg-bookmark-card {
+            margin: 1rem 0;
+            width: 100%;
+
+            a.kg-bookmark-container {
+                box-shadow: 0 2px 5px -1px rgba(0,0,0,.15), 0 0 1px rgba(0,0,0,.09);
+                color: #15171a;
+                display: flex;
+                min-height: 148px;
+                text-decoration: none;
+
+                @media (max-width: 500px) {
+                    flex-direction: column;
+                }
+
+                div.kg-bookmark-content {
+                    display: flex;
+                    flex-grow: 1;
+                    flex-direction: column;
+                    justify-content: flex-start;
+                    align-items: flex-start;
+                    padding: 20px;
+
+                    @media (max-width: 500px) {
+                        order: 2;
+                    }
+    
+                    div.kg-bookmark-title {
+                        font-weight: 600;
+                        font-size: ${theme.fontSize.base};
+
+                        @media (max-width: 500px) {
+                            font-size: ${theme.fontSize.sm};
+                        }
+                    }
+                    div.kg-bookmark-description {
+                        overflow-y: hidden;
+                        margin-top: 12px;
+                        max-height: 48px;
+                        color: #5d7179;
+                        line-height: 1.5em;
+                        font-weight: 400;
+                        font-size: ${theme.fontSize.base};
+
+                        @media (max-width: 500px) {
+                            font-size: ${theme.fontSize.sm};
+                        }
+                    }
+                    div.kg-bookmark-metadata {
+                        display: flex;
+                        flex-wrap: wrap;
+                        align-items: center;
+                        margin-top: 14px;
+                        color: #5d7179;
+                        font-size: ${theme.fontSize.base};
+                        font-weight: 400;
+
+                        @media (max-width: 500px) {
+                            font-size: ${theme.fontSize.sm};
+                            line-height: 1.5em;
+                        }
+
+                        img.kg-bookmark-icon {
+                            margin-right: 8px;
+                            width: 22px;
+                            height: 22px;
+
+                            @media (max-width: 500px) {
+                                width: 18px;
+                                height: 18px;
+                            }
+                        }
+                        span.kg-bookmark-publisher {
+                            overflow: hidden;
+                            max-width: 240px;
+                            line-height: 1.5em;
+                            text-overflow: ellipsis;
+                            white-space: nowrap;
+                        }
+                    }
+                }
+
+                div.kg-bookmark-thumbnail {
+                    position: relative;
+                    min-width: 33%;
+                    min-height: 160px;
+                    max-height: 100%;
+
+                    @media (max-width: 500px) {
+                        width: 100%;
+                        min-height: 160px;
+                        order: 1;
+                    }
+
+                    img {
+                        position: absolute;
+                        top: 0;
+                        left: 0;
+                        width: 100%;
+                        height: 100%;
+                        object-fit: cover;
+                        order: 1;
+                        border-radius: 0 3px 3px 0;
+                    }
+                }
+            }
+        }
+    }
+
     blockquote {
         border-left: 3px solid #3eb0ef;
+        margin: 1.5rem 0;
         padding: 0 0 0 1.5rem;
         
         &:before {
@@ -124,7 +262,7 @@ const StyledPostContent = styled.section`
     }
 
     p {
-        padding-bottom: 1rem;
+        margin: 0.5rem 0;
     }
 
     a {
@@ -138,10 +276,10 @@ const StyledPostContent = styled.section`
 
     ol, ul {
         list-style-position: outside;
-        padding-left: 1.5rem;
+        margin-left: 1.5rem;
 
         li {
-            padding-bottom: 0.5rem;
+            margin: 0.3rem 0;
         }
     }
     ol {
