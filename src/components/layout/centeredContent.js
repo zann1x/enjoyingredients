63b2eb3d659497931d6f3fb3d732b507b7696f01
Lukas Zanner <git@lukaszanner.de>
2020-03-26 16:53:04 +0100

    Simplify overly complicated layout setup

--- src/components/layout/centeredContent.js
+++ src/components/layout/centeredContent.js
@@ -1,15 +1,38 @@
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import styled from "styled-components";
 
-import SiteLayout from "~components/layout/siteLayout";
+import Footer from "~components/footer";
+import Navbar from "~components/navbar";
 
 export const CenteredContent = ({children}) => {
+    const { site } = useStaticQuery(
+        graphql`
+            query {
+                site {
+                    siteMetadata {
+                        title
+                    }
+                }
+            }
+        `
+    );
+
     return (
-        <SiteLayout>
+        <StyledSiteWrapper>
+            <Navbar siteTitle={site.siteMetadata.title}></Navbar>
             <div className="container mx-auto flex-grow px-2 py-4 w-11/12 sm:w-10/12 md:w-8/12 lg:w-7/12 xl:w-6/12">
                 {children}
             </div>
-        </SiteLayout>
+            <Footer></Footer>
+        </StyledSiteWrapper>
     );
 }
 
 export default CenteredContent;
+
+const StyledSiteWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+`;
