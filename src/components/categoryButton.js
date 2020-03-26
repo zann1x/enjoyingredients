c199fc5290ca4d62378848290a55a4d13641c7e2
Lukas Zanner <git@lukaszanner.de>
2020-03-26 19:59:37 +0100

    Update category link in post and teaser

--- src/components/categoryButton.js
+++ src/components/categoryButton.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Link } from "gatsby";
 import PropTypes from "prop-types";
-import { createPathFromSlug, EUrlType } from "~utils/createLinkFromSlug";
+import { createIdLinkFromSlug, EUrlType } from "~utils/createLinkFromSlug";
 
 const CategoryButton = ({ category }) => {
-    const categoryUrl = createPathFromSlug(EUrlType.BLOG_CATEGORY, category.slug);
+    const categoryUrl = createIdLinkFromSlug(EUrlType.BLOG_CATEGORY, category.slug);
     return (
         <Link to={categoryUrl} className="inline-block rounded-full px-3 py-1 mx-1 text-sm font-semibold text-gray-700 bg-gray-200">
             {category.name}
