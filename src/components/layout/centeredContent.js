ceb053eccdf540587770763c051290ccc0f96714
Lukas Zanner <git@lukaszanner.de>
2020-03-28 00:26:50 +0100

    Extract centered content layout from general site layout

--- src/components/layout/centeredContent.js
+++ src/components/layout/centeredContent.js
@@ -1,38 +1,11 @@
 import React from "react";
-import { useStaticQuery, graphql } from "gatsby";
-import styled from "styled-components";
-
-import Footer from "~components/footer";
-import Navbar from "~components/navbar";
 
 export const CenteredContent = ({children}) => {
-    const { site } = useStaticQuery(
-        graphql`
-            query {
-                site {
-                    siteMetadata {
-                        title
-                    }
-                }
-            }
-        `
-    );
-
     return (
-        <StyledSiteWrapper>
-            <Navbar siteTitle={site.siteMetadata.title}></Navbar>
-            <div className="container mx-auto flex-grow px-2 py-4 w-11/12 sm:w-10/12 md:w-8/12 lg:w-7/12 xl:w-6/12">
-                {children}
-            </div>
-            <Footer></Footer>
-        </StyledSiteWrapper>
+        <div className="container mx-auto flex-grow px-2 py-4 w-11/12 sm:w-10/12 md:w-8/12 lg:w-7/12 xl:w-6/12">
+            {children}
+        </div>
     );
 }
 
 export default CenteredContent;
-
-const StyledSiteWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-`;
