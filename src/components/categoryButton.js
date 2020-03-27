fecac05b379f157df856cfbcb336073d61430e51
Lukas Zanner <git@lukaszanner.de>
2020-03-27 23:05:41 +0100

    Fix spacing for overflowing category buttons

--- src/components/categoryButton.js
+++ src/components/categoryButton.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "gatsby";
 import PropTypes from "prop-types";
+
 import { createIdPathFromSlug, EUrlType } from "~utils/createPathFromSlug";
 
 const CategoryButton = ({ category }) => {
     const categoryPath = createIdPathFromSlug(EUrlType.BLOG_CATEGORY, category.slug);
     return (
-        <Link to={categoryPath} className="inline-block rounded-full px-3 py-1 mx-1 text-sm font-semibold text-gray-700 bg-gray-200">
+        <Link to={categoryPath}
+            className="inline-block rounded-full px-3 py-1 m-1 text-sm font-semibold text-gray-700 bg-gray-200">
             {category.name}
         </Link>
     );
