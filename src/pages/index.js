8f73feb330cd50960b9c15be54573ec68bf0a62f
Lukas Zanner <git@lukaszanner.de>
2020-04-09 19:14:36 +0200

    Add more posts button on the startpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,26 @@
 import React from "react";
 import { graphql } from "gatsby";
+import { Link, useIntl } from "gatsby-plugin-intl";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 
-import Footer from "~components/footer";
-import Navbar from "~components/navbar";
 import PostTeaserCard from "~components/postTeaserCard";
 import SEO from "~components/seo";
+import SiteLayout from "~components/layout/siteLayout";
 import config from '~utils/config';
+import { EUrlType } from '~utils/createPathFromSlug';
+import theme from '~styles/theme';
 
-export const Index = ({ data: { site, latestPosts }}) => {
+export const Index = ({ data: { latestPosts }}) => {
+    const intl = useIntl();
+
     return (
-        <>
+        <SiteLayout>
             <SEO
                 title="The other food blog"
                 description={config.siteDescription}
                 pathname="/"
             />
-            <Navbar siteTitle={site.siteMetadata.title} />
 
             <div className="container mx-auto px-2 py-6 w-11/12">
                 {latestPosts.nodes.map((post) => {
@@ -28,13 +32,16 @@ export const Index = ({ data: { site, latestPosts }}) => {
                 })}
             </div>
 
-            <Footer></Footer>
-        </>
+            <StyledMoreButton to={EUrlType.BLOG_CATEGORY}>
+                {/* <Link to={EUrlType.BLOG_CATEGORY}> */}
+                    {intl.formatMessage({ id: 'startpage_more_posts' })}
+                {/* </Link> */}
+            </StyledMoreButton>
+        </SiteLayout>
     );
 }
 
 Index.propTypes = {
-    site: PropTypes.object,
     latestPosts: PropTypes.arrayOf(PropTypes.object),
 };
 
@@ -42,12 +49,6 @@ export default Index;
 
 export const pageQuery = graphql`
     query {
-        site {
-            siteMetadata {
-                title
-            }
-        }
-
         latestPosts: allGhostPost(
             sort: {
                 order: DESC,
@@ -68,6 +69,22 @@ export const pageQuery = graphql`
                     name
                 }
             }
+        }
+    }
+`;
+
+const StyledMoreButton = styled(Link)`
+    border: 1px solid ${theme.color.gray600};
+    border-radius: 1.5rem;
+    margin: 0 auto 1.5rem;
+    padding: 0.75rem 1.5rem;
+    text-align: center;
+
+    &:hover {
+        background-color: ${theme.color.gray100};
+
+        &:focus {
+            background-color: ${theme.color.gray200};
         }
     }
 `;
