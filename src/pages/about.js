4788637be83d017eab7ce433787324e9a3ee8bd5
Lukas Zanner <git@lukaszanner.de>
2020-07-01 11:37:12 +0200

    Query about page content from Ghost

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { graphql } from "gatsby";
-import { useIntl } from "gatsby-plugin-intl";
+import { graphql, useStaticQuery } from "gatsby";
 import styled from "styled-components";
 
 import CenteredContent from "~components/layout/centeredContent";
@@ -8,55 +7,56 @@ import SiteLayout from "~components/layout/siteLayout";
 import SEO from "~components/seo";
 import theme from "~styles/theme";
 
-export default ({ data, location }) => {
-    const { aboutTranslations } = data;
+export default ({ location }) => {
+    const qdata = useStaticQuery(graphql`
+        query {
+            ghostPage(
+                slug: { eq: "ueber-mich" },
+                # tags: { elemMatch: { slug: { eq: $tagslug } } }
+            ) {
+                title
+                excerpt
+                slug
+                html
+                tags { slug }
+            }
+        }
+    `);
+    console.log(qdata);
+
+    // const { aboutTranslations } = data;
 
     // Get the content in the language we are currently visiting the site with
-    const intl = useIntl();
-    const content = aboutTranslations.nodes.filter(page => intl.locale === page.frontmatter.language)[0];
+    // const intl = useIntl();
+    // const content = aboutTranslations.nodes.filter(page => intl.locale === page.frontmatter.language)[0];
+    const content = qdata.ghostPage;
 
     return (
         <SiteLayout>
             <SEO
-                title={content.frontmatter.title}
-                description={content.frontmatter.description}
+                title={content.title}
+                description={content.excerpt}
                 pathname={location.pathname} />
             <CenteredContent>
-                <StyledHeading>{content.frontmatter.title}</StyledHeading>
-                <section className="leading-relaxed text-justify" dangerouslySetInnerHTML={{ __html: content.html }}></section>
+                <StyledHeading>{content.title}</StyledHeading>
+                <StyledContentSection dangerouslySetInnerHTML={{ __html: content.html }}></StyledContentSection>
             </CenteredContent>
         </SiteLayout>
     );
 }
-
-export const pageQuery = graphql`
-    query {
-        aboutTranslations: allMarkdownRemark(
-            filter: {
-                fields: {
-                    slug: {
-                        regex: "//about//"
-                    }
-                }
-            }) {
-            nodes {
-                fields {
-                    slug
-                }
-                frontmatter {
-                    title
-                    description
-                    language
-                }
-                html
-            }
-        }
-    }
-`;
 
 const StyledHeading = styled.h1`
     font-size: ${theme.fontSize.h1};
     font-weight: ${theme.fontWeight.f700};
 
     padding-bottom: 0.5rem;
+`;
+
+const StyledContentSection = styled.section`
+    line-height: 1.625;
+    text-align: justify;
+
+    p {
+        margin: 0.5rem 0;
+    }
 `;
