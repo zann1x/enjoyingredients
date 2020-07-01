4f28b65d92ae4da30fea32742173e27d5c49f188
Lukas Zanner <git@lukaszanner.de>
2020-07-01 15:02:04 +0200

    Move styles on about page in separate file

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
-import styled from "styled-components";
 
 import CenteredContent from "~components/layout/centeredContent";
 import SiteLayout from "~components/layout/siteLayout";
 import SEO from "~components/seo";
-import theme from "~styles/theme";
+
+import * as Styled from "./about.styled";
 
 export default ({ location }) => {
     const data = useStaticQuery(graphql`
@@ -37,25 +37,9 @@ export default ({ location }) => {
                 description={content.excerpt}
                 pathname={location.pathname} />
             <CenteredContent>
-                <StyledHeading>{content.title}</StyledHeading>
-                <StyledContentSection dangerouslySetInnerHTML={{ __html: content.html }}></StyledContentSection>
+                <Styled.Heading>{content.title}</Styled.Heading>
+                <Styled.ContentSection dangerouslySetInnerHTML={{ __html: content.html }}></Styled.ContentSection>
             </CenteredContent>
         </SiteLayout>
     );
 }
-
-const StyledHeading = styled.h1`
-    font-size: ${theme.fontSize.h1};
-    font-weight: ${theme.fontWeight.f700};
-
-    padding-bottom: 0.5rem;
-`;
-
-const StyledContentSection = styled.section`
-    line-height: 1.625;
-    text-align: justify;
-
-    p {
-        margin: 0.5rem 0;
-    }
-`;
