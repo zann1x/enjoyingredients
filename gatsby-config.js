acadc0104330143da6ff552610984952522f18a8
Lukas Zanner <git@lukaszanner.de>
2020-08-22 20:42:58 +0200

    Add Ghost images plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,36 @@ module.exports = {
         'gatsby-plugin-styled-components',
         'gatsby-transformer-sharp',
         {
+            resolve: `gatsby-plugin-ghost-images`,
+            options: {
+                // An array of node types and image fields per node
+                // Image fields must contain a valid absolute path to the image to be downloaded
+                lookup: [
+                    {
+                        type: `GhostPost`,
+                        imgTags: [`feature_image`],
+                    },
+                    {
+                        type: `GhostPage`,
+                        imgTags: [`feature_image`],
+                    },
+                    {
+                        type: `GhostSettings`,
+                        imgTags: [`cover_image`],
+                    },
+                ],
+                // Additional condition to exclude nodes
+                // Takes precedence over lookup
+                exclude: node => (
+                    node.ghostId === undefined
+                ),
+                // Additional information messages useful for debugging
+                verbose: true,
+                // Option to disable the module (default: false)
+                disable: false,
+            },
+        },
+        {
             resolve: `gatsby-plugin-intl`,
             options: {
                 // language JSON resource path
