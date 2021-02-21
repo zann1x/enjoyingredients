4b6ab445134f482aa97a8fe8be4a9021fd6b0469
Lukas Zanner <git@lukaszanner.de>
2021-02-21 22:24:53 +0100

    Add ghost API lib

---
+++ src/lib/ghost-api.ts
@@ -0,0 +1,66 @@
+import GhostContentAPI from '@tryghost/content-api';
+
+const api = new GhostContentAPI({
+    url: process.env.GHOST_API_URL,
+    key: process.env.GHOST_API_KEY,
+    version: 'v3',
+});
+
+export async function getPostsForIndexPage() {
+    const posts = await api.posts.browse({
+        order: 'published_at DESC',
+        include: 'tags',
+    });
+
+    return posts;
+}
+
+export async function getAllPostsWithTags() {
+    const posts = await api.posts.browse({
+        order: 'published_at DESC',
+        include: 'tags'
+    });
+
+    return posts;
+};
+
+export async function getAllTagsWithPosts() {
+    const tags = await api.tags.browse({
+        filter: 'posts.count:>0',
+        order: 'slug ASC'
+    });
+
+    return tags;
+};
+
+export async function getAllPostsRaw() {
+    const posts = await api.posts.browse({
+        order: 'published_at DESC'
+    });
+
+    return posts;
+};
+
+export async function getAllPagesRaw() {
+    const pages = await api.pages.browse();
+
+    return pages;
+}
+
+export async function getPostBySlug(slug) {
+    const post = await api.posts.read({
+        slug: slug,
+        include: 'tags'
+    });
+
+    return post;
+}
+
+export async function getPageBySlug(slug) {
+    const page = await api.pages.read({
+        slug: slug,
+        include: 'tags'
+    });
+
+    return page;
+}
