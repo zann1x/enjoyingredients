a7c4d7da0db48b2940cab0004092058d223159ff
Lukas Zanner <git@lukaszanner.de>
2021-02-21 22:24:17 +0100

    Add next.config.js with custom TLD domain for germany

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.export = {
+    compress: false,
+    i18n: {
+        locales: ['de'],
+        defaultLocale: 'de',
+        domains: [
+            {
+                domain: 'enjoyingredients.de',
+                defaultLocale: 'de',
+            },
+        ],
+    },
+    reactStrictMode: true,
+};
